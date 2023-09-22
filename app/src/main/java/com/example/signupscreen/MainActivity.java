5347a26f4eb9d56f64f624e6f9c4f2eb35328405
Đào Đức Mạnh <manhdao1006@gmail.com>
2023-09-22 21:30:58 +0700

    first commit

---
+++ app/src/main/java/com/example/signupscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.signupscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
